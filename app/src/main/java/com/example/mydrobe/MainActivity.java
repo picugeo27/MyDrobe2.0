6efae5dfbf90250e2154622b0378f2a9b0d95a7d
adrian <adrianpenaga@gmail.com>
2021-10-13 08:47:24 +0200

    13/10 adri

--- app/src/main/java/com/example/mydrobe/MainActivity.java
+++ app/src/main/java/com/example/mydrobe/MainActivity.java
@@ -4,28 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int mCounter = 0;
-    Button btn;
-    TextView txv;
+    TextView txv,coin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button) findViewById (R.id.bt);
-        txv = (TextView) findViewById (R.id.tx);
     }
-    public void sendMessage(View view) {
-        setContentView(R.layout.interfaztienda);
-    }
-    public void cliker(View view) {
-        mCounter++;
+
+    public void recuperarP(View view){
+        setContentView(R.layout.activity_main);
+        txv = (TextView) findViewById (R.id.tx_puntos);
         txv.setText(Integer.toString(mCounter));
     }
-    public void back(View view) {
-        setContentView(R.layout.activity_main);
+
+    public void irTienda(View view) {
+        setContentView(R.layout.interfaztienda);
+        coin = (TextView) findViewById (R.id.coins);
+        coin.setText(Integer.toString(mCounter));
     }
+
+    public void cliker(View view) {
+        mCounter++;
+        txv = (TextView) findViewById (R.id.tx_puntos);
+        txv.setText(Integer.toString(mCounter));
+    }
+
 }
