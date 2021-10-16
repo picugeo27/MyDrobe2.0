079857349f41f15025f2bf8e2cda6d7d37bfa9ff
ManiVP <ultramanivk@gmail.com>
2021-10-16 20:00:27 +0200

    Serializar usuario hecho.

--- app/src/main/java/com/example/mydrobe/MainActivity.java
+++ app/src/main/java/com/example/mydrobe/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.mydrobe;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
+    Context context = this;
+    File fichero = new File("/data/user/0/com.example.mydrobe/files/usuarioUnico.bat");
     int modo = 0;
     ArrayList<String> poolFrasesNormales = new ArrayList<>();
     ArrayList<String> poolFrasesObscenas = new ArrayList<>();
     Usuario usuario = new Usuario();
 
+    int requestCode = 200;
     int mlt=1;
     TextView txPuntos;
 
@@ -29,12 +44,98 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verificarPermisos();
+
         txPuntos = (TextView) findViewById (R.id.tx_puntos);
+            try {
+                inicializarSistema();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
         try {
-            anadirFrases("normal", poolFrasesNormales);
+            guardarUsuario();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void verificarPermisos() {
+        int permES = ContextCompat.checkSelfPermission( context , Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permEW = ContextCompat.checkSelfPermission( context , Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (PackageManager.PERMISSION_GRANTED == permES && PackageManager.PERMISSION_GRANTED == permEW){
+            Toast.makeText( context, "Permiso garanted", Toast.LENGTH_SHORT).show();
+        }else{
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},this.requestCode);
+        }
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==event.KEYCODE_BACK){
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setMessage("¿Deseas salis de MYDrove?")
+                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent=new Intent(Intent.ACTION_MAIN);
+                            intent.addCategory(Intent.CATEGORY_HOME);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            try {
+                                guardarUsuario();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    })
+                    .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    builder.show();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void guardarUsuario() throws IOException {
+        ObjectOutputStream oos = null;
+        oos = new ObjectOutputStream(new FileOutputStream(fichero));
+        oos.writeObject(usuario);
+        oos.close();
+    }
+
+    private void inicializarSistema() throws IOException, ClassNotFoundException { //Cargamos el ususario y las frases en el MainActivity..
+        //Cargamos el usuario
+        long files = fichero.length();
+        boolean x = files == 0;
+        if(!x){
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichero));
+            this.usuario = (Usuario) ois.readObject();
+            ois.close();
+        }
+        //Cargamos las frases en los arrays correspondientes.
+        String linea = null;
+        BufferedReader br = new BufferedReader(new FileReader("FrasesNormales.txt"));
+        while((linea = br.readLine()) != null) {
+            this.poolFrasesNormales.add(linea);
+
+            Toast.makeText( context, linea, Toast.LENGTH_SHORT).show();
+        }
+        br.close();
+
     }
 
     public void cliker(View view) {
@@ -43,11 +144,11 @@ public class MainActivity extends AppCompatActivity {
 
         int cont = usuario.getContador();
         if ((cont%10)==0){
-            FraseAleatoria(poolFrasesNormales);
+            fraseAleatoria(usuario.getPoolfrasesNormales()); /*¿No deberia coger las frases del ususario?*/
         }
     }
 
-    public void FraseAleatoria(@NonNull ArrayList<String> poolFrases) {
+    public void fraseAleatoria(@NonNull ArrayList<String> poolFrases) {
         int RangoAleatorio = poolFrases.size();
         int numeroAleatorio = (int) (Math.random() * RangoAleatorio);
         String FraseMostrar = poolFrases.get(numeroAleatorio);
@@ -124,7 +225,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public static void anadirFrases(String TipoFrase, ArrayList<String> poolFrases) throws FileNotFoundException, IOException {
+    /*public static void anadirFrases(String TipoFrase, ArrayList<String> poolFrases) throws FileNotFoundException, IOException {
         String direccionArchivo="";
         if (TipoFrase =="normal"){
             direccionArchivo = "FrasesNormales.txt" ;
@@ -138,7 +239,7 @@ public class MainActivity extends AppCompatActivity {
             poolFrases.add(cadena);
         }
         b.close();
-    }
+    }*/
 
     public void MejorarClicks(View view){
         if(usuario.pago(usuario.getValorClick()*10)){
@@ -149,6 +250,5 @@ public class MainActivity extends AppCompatActivity {
             mySnackbar.show();
         }
     }
-
 
 }
